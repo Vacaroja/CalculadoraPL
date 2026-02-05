77ffe043db7f2dd74fa71ad87a0255e2126bc8e3
Andrés Alfonzo <vr.alfonzo1@gmail.com>
2026-02-04 23:14:13 -0400

    archivo app.py con la ruta de resultado

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from data.funciones.Ordenador import ordenador
 from flask import Flask, render_template, request, redirect, url_for, session,jsonify
 
 
@@ -14,13 +15,38 @@ def index():
 @app.route('/restricciones', methods=['POST'])
 def restricciones():
     metodo = request.form.get('metodo')
+    cantVar = request.form.get('CantVar')
+    print(cantVar)
+    print(metodo)
     try:
         cantVar = int(request.form.get('CantVar'))
     except:
         cantVar = 0
+    return render_template('restricciones.html', cantVar=cantVar, metodo=metodo)
 
+@app.route('/resultados', methods=['POST'])
+def resultados():
+    metodo = request.form.get('metodo')
+    cantVar = request.form.get('CantVar')
+    datos_variables = {}
+    for key in request.form.keys():
+        if key.startswith('x'):
+            # getlist extrae todos los inputs con el mismo nombre (las filas añadidas)
+            datos_variables[key] = request.form.getlist(key)    
+    # También capturamos los resultados (lo que está después del =)
+    resultados = request.form.getlist('res[]')
+    datos_variables['res'] = request.form.getlist('res[]') 
+    listaf = ordenador(datos_variables)
+    for e in listaf:
+        print(e)
     
-    return render_template('restricciones.html',metodo = metodo,cantVar = cantVar)
+
+    print(f"Variables capturadas: {datos_variables}")
+    print(f"Resultados (Sol): {resultados}")
+
+    print("Datos recibidos correctamente")
+    
+    return render_template('resultados.html')
 
     
 
